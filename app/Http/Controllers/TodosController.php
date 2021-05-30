c2db3fc8acf64038d2411205c38a9c86f2d0e387
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-05-30 20:12:10 +0900

    first

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Todo::all();
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new Todo;
+        $item->content = $request->content;
+        $item->save();
+        return response()->json([
+            'message' => "OK",
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $item = Todo::where('id', $todo->id)->first();
+        $item->content = $request->content;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => 'Todo-List Updata successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not Found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $item = Todo::where('id', $todo->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => 'Todo-List Not found',
+            ], 404);
+        }
+    }
+}
